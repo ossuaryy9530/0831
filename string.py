6a4466d09721991557e480f01b31050d5d64c1b2
ossuaryy9530 <55948166+ossuaryy9530@users.noreply.github.com>
2019-11-16 10:35:32 +0900

    Add files via upload

---
+++ string.py
@@ -0,0 +1,13 @@
+print("{:s}".format('1\'m enjoying learning Python'))
+
+print("{0:s}".format("This is a long string Withust the backslash\
+it would run off of the page on th night in the textt editor and be very\
+difficult in read and edit. By using the backslash you can split the long\
+string nto smaller strings on seperate lines so that the wole string is easy\
+to view in the editor"))
+
+print("{0:s}".format('''You can use triple single quotes 
+for multi-line comment strings'''))
+
+print("{:s}".format("""You can also use triple double quotes
+for multi-line comment strings."""))
